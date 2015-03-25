fe4d23c7e1de52e3933271927e844171326fd3c6
Carlos Jordan <jjordan84@gmail.com>
2015-03-25 15:35:55 -0500

    Fixes on SQL Statements
    Modify create procedures as functions and minor
    syntax corrections

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -10,18 +10,17 @@ CREATE DATABASE  tournament;
 
 CREATE TABLE Players(PlayerID SERIAL PRIMARY KEY,FirstName varchar(255),Wins int,Matches int);
 
-CREATE TABLE Matches(
-    MatchID SERIAL PRIMARY KEY,
-    Winner int,
-    Loser int
-);
+CREATE TABLE Matches( MatchID SERIAL PRIMARY KEY, Winner int,Loser int);
 
 CREATE VIEW number_players AS SELECT COUNT(*) FROM Players;
-CREATE PROCEDURE delete_Players AS DELETE * FROM Players;
-CREATE PROCEDURE delete_Matches AS DELETE * FROM Matches;
-CREATE PROCEDURE insert_Players AS @FirstName nvarchar(30) = NULL AS INSERT INTO Players(FirstName,Wins,Matches) VALUES (@FirstName,0,0);
-CREATE PROCEDURE return_PLayers AS SELECT * from Players ORDER BY Wins ASC , Matches DESC;
-CREATE PROCEDURE report_match AS @winner int,@loser int
-AS INSERT INTO Matches(Winner,Loser) VALUES (@winner,@loser)
-AS UPDATE TABLE PLAYERS set Matches = Matches + 1 WHERE PlayerID = @winner OR PLAYERID =@loser ;
 CREATE VIEW list_players AS SELECT PlayerID,FirstName,Wins from Players order by Wins ASC;
+
+CREATE FUNCTION delete_Players() RETURNS void  AS $$ DELETE FROM Players;$$ LANGUAGE SQL;
+CREATE FUNCTION delete_Matches() RETURNS void AS $$ DELETE FROM Matches;$$ LANGUAGE SQL;
+CREATE FUNCTION insert_Players(name varchar(30)) returns void AS $$  INSERT INTO Players(FirstName,Wins,Matches) VALUES (name,0,0); $$ LANGUAGE SQL;
+CREATE FUNCTION return_Players(out id int,out name varchar(30),out win int,out los int) RETURNS SETOF record AS $$ SELECT * from Players ORDER BY Wins DESC; $$ LANGUAGE SQL;
+CREATE FUNCTION report_match(in winner int,in loser int) RETURNS VOID AS $$
+INSERT INTO Matches(Winner,Loser) VALUES (Winner,Loser);
+UPDATE Players set Wins = Wins + 1 WHERE PlayerID = Winner;
+UPDATE Players set Matches = Matches + 1 WHERE PlayerID = Winner OR PlayerID = Loser;
+$$ LANGUAGE SQL;
