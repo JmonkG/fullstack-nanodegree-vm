79d8f149b635d0aa859ac69f35895837a2d066c9
Carlos Jordan <jjordan84@gmail.com>
2015-04-09 15:47:31 -0500

    Flesk and SQLAlchemy integration ready
    This is done in FleskAlch.py

--- vagrant/catalog/FleskAlch.py
+++ vagrant/catalog/FleskAlch.py
@@ -2,15 +2,47 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
-class Dog(db.Model):
-    id = db.Column(db.Integer, primary_key = 'TRUE')
-    name = db.Column(db.String(80))
-    race = db.Column(db.String(80))
+        
+class User(db.Model):
     
-    def __init__(self,name,race):
+    __tablename__ = 'User'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(250),nullable = False)
+    password = db.Column(db.String(250),nullable = False)
+    email_address = db.Column(db.String(250),nullable = False)
+    
+    def __init__(self,name,password,email):
         self.name = name
-        self.race = race
+        self.password = password
+        self.email_address = email
+
+class Category(db.Model):
     
+    __tablename__ = 'Category'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(250),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('User.id'))
+    user = db.relationship('User')
+    
+    def __init__(self,name,user):
+        self.name = name
+        self.user = user
+
+class Item(db.Model):
+    
+    __tablename__ = 'Item'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    description = db.Column(db.String(500))
+    category_id = db.Column(db.Integer,db.ForeignKey('Category.id'))
+    category = db.relationship('Category')
+    
+    def __init__(self,name,description,category):
+        self.name = name
+        self.description = description
+        self.category = category
