bba8d48fbffd6e64c5e869993eb9b5d2229ba820
Carlos Jordan <jjordan84@gmail.com>
2015-04-15 17:59:18 -0500

    Cleaning some files
    putting some files on folders and cleaning to start the final project :)

---
+++ vagrant/catalog/db_setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy_imageattach.entity import Image,image_attachment
+
+Base = declarative_base()
+
+class Category(Base):
+    
+    __tablename__ = 'Category'
+    
+    id = Column(Integer,primary_key=True)
+    name = Column(String(80),nullable=False)
+    description = Column(String(250),nullable=False)
+
+class Item(Base):
+    __tablename__ = 'Item'
+    
+    id = Column(Integer,primary_key=True)
+    name = Column(String(80),nullable=False)
+    description = Column(String(250),nullable=False)
+    image = image_attachment('ItemPict')
+    category_id = Column(Integer,ForeignKey('Category.id'))
+    category = relationship('Category')
+
+class ItemPict(Base):    
+    __tablename = 'ItemPicture'
+    
+    item_id = Column(Integer,ForeigKey('Item.id'),primary_key = True)
+    item = relationship('Item')
+    
+
+engine = create_engine('sqlite:///Catalog.db')
+Base.metadata.create_all(engine)
---
+++ vagrant/catalog/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,url_for,redirect,request,flash,jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base,Category,Item,ItemPict
+from sqlalchemy_imageattach.stores.fs import httpExposedFileSystemStore
+
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///Catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+fs_store = HttpExposedFileSystemStore('itemimages','images/')
+app.wsgi_app = fs_store.wsgi_middleware(app.wsgi_app)
