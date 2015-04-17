adfc17740f1d7f9bcb42f7dd8428a1713de3eb72
Carlos Jordan <jjordan84@gmail.com>
2015-04-17 16:58:43 -0500

    Flask-Uploads working :)
    use sudo pip install Flask-Uploads - -upgrade

--- vagrant/catalog/main.py
+++ vagrant/catalog/main.py
@@ -1,46 +1,44 @@
 import os
 from flask import Flask,render_template,url_for,redirect,request,flash,jsonify
+from flask.ext.uploads import UploadSet,IMAGES,configure_uploads
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base,Category,Item
 from werkzeug import secure_filename
 
-UPLOADED_FILES_DEST= os.getcwd()+'/images'
-photos = UploadSet('photos',IMAGES)
-#ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-#from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
-#from sqlalchemy_imageattach.context import store_context
 
-#HttpExposedFileSystemStore(
-#    path='fullstack-nanodegree-vm/vagrant/catalog/images',
-#    prefix='images/'
-#)
 app = Flask(__name__)
+app.config['UPLOADED_PHOTOS_DEST']= os.getcwd()+'/images'
+photos = UploadSet('photos',IMAGES)
+configure_uploads(app,photos)
+
+#OPEN DB PARAMETERS
 engine = create_engine('sqlite:///Catalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#fs_store = HttpExposedFileSystemStore('itempics','images/')
-#app.wsgi_app = fs_store.wsgi_middleware(app.wsgi_app)
+
 
 
 @app.route('/')
 @app.route('/Item/new/',methods=['GET','POST'])
 def addItem():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.form['photo'])
-        rec= Photo()
-        newitem = Item(name=request.form['name'],description='whatevs')
+    if request.method == 'POST':
+        filename =photos.save(request.files['picture'])
+        newitem = Item(name=request.form['name'],description='whatevs',image_name=filename)
         session.add(newitem)
         session.commit()
-        return redirect(url_for('show_item',id_item=1))
+        return redirect(url_for('show_item',id_item=newitem.id))
     else:
         return render_template('newitem.html')
 
 @app.route('/Item/<int:id_item>/')
 def show_item(id_item):
+    ex_item = session.query(Item).filter_by(id=id_item).one()
+    url = photos.url(ex_item.image_name)
+    return render_template("Create_menu.html",item=ex_item,direc=url)
   
 
 if __name__ == '__main__':
