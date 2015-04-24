968ca83064c8577e06159f03ebe9fc0f6a944173
Carlos Jordan <jjordan84@gmail.com>
2015-04-24 16:07:04 -0500

    changed url using names instead of ids

--- vagrant/catalog/main.py
+++ vagrant/catalog/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask,render_template,url_for,redirect,request,flash,jsonify
 from flask.ext.uploads import UploadSet,IMAGES,configure_uploads
-from sqlalchemy import create_engine,desc
+from sqlalchemy import create_engine,desc,and_
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base,Category,Item
 from werkzeug import secure_filename
@@ -21,93 +21,60 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/Categories/')
+@app.route('/catalog/')
 def categories():
     categories = session.query(Category).all()
     select_last_items()
     return render_template('categories.html',categories=categories)
 
-@app.route('/Categories/new',methods=['GET','POST'])
-def addCategory():
-    if request.method == 'POST':
-        new_category = Category(name=request.form['name'],description=request.form['description'])
-        session.add(new_category)
-        session.commit()
-        return redirect(url_for('categories'))
-    else:
-        return render_template('newcategory.html')
-    
-@app.route('/Categories/edit/<int:id_category>/',methods=['GET','POST'])
-def editCategory(id_category):
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        category = session.query(Category).filter_by(id=id_category).one()
-        category.name = name
-        category.description = description
-        session.add(category)
-        session.commit()
-        return redirect(url_for('categories'))
-    else:
-        category= session.query(Category).filter_by(id=id_category).one()
-        return render_template('editCategory.html',category=category)
-    
-@app.route('/Categories/delete/<int:id_category>', methods=['GET','POST'])
-def deleteCategory(id_category):
-    category = session.query(Category).filter_by(id=id_category).one()
-    session.delete(category)
-    session.commit()
-    return redirect('Categories')
+@app.route('/catalog/<category_name>/Items/')
+def Items(category_name):
+    cat = session.query(Category).filter_by(name=category_name).one();
+    items = session.query(Item).filter_by(category_id=cat.id).all()
+    return render_template('items.html',items=items,category_name=category_name)
 
-@app.route('/Categories/<int:id_category>/Items/')
-def Items(id_category):
-    items = session.query(Item).filter_by(category_id=id_category).all()
-    return render_template('items.html',items=items,id_category=id_category)
-
-@app.route('/Categories/<int:id_category>/Items/new/',methods=['GET','POST'])
-def addItem(id_category):
+@app.route('/catalog/<category_name>/Items/new/',methods=['GET','POST'])
+def addItem(category_name):
     if request.method == 'POST':
+        cat = session.query(Category).filter_by(name=category_name).one()
         filename =photos.save(request.files['picture'])
-        newitem = Item(name=request.form['name'],description=request.form['description'],image_name=filename,category_id=id_category)
+        newitem = Item(name=request.form['name'],description=request.form['description'],image_name=filename,category_id=cat.id)
         session.add(newitem)
         session.commit()
-        return redirect(url_for('Items',id_category=id_category))
+        return redirect(url_for('Items',category_name=category_name))
     else:
-        return render_template('newitem.html',id_category=id_category)
+        cat = session.query(Category).filter_by(name=category_name).one()
+        return render_template('newitem.html',id_category=cat.id,category_name=category_name)
     
 @app.route('/')
-@app.route('/Categories/<int:id_category>/Items/edit/<int:id_item>/',methods=['GET','POST'])
-def editItem(id_item,id_category):
+@app.route('/catalog/<category_name>/Items/edit/<item_name>/',methods=['GET','POST'])
+def editItem(item_name,category_name):
     if request.method == 'POST':
+        cat = session.query(Category).filter_by(name=category_name).one()
         filename = photos.save(request.files['picture'])
         name = request.form['name']
         description = request.form['description']
-        item = session.query(Item).filter_by(id=id_item).one()
+        item = session.query(Item).filter(and_(Item.name==item_name,Item.category_id==cat.id)).one()
         item.name = name
         item.description = description
         item.image_name = filename
         session.add(item)
         session.commit()
-        return redirect(url_for('Items',id_category=id_category))
+        return redirect(url_for('Items',category_name=category_name))
     else:
-        item_edit = session.query(Item).filter_by(id=id_item).one()
+        cat = session.query(Category).filter_by(name=category_name).one()
+        item_edit = session.query(Item).filter_by(name=item_name).one()
         url = photos.url(item_edit.image_name)
-        return render_template('editItem.html',item=item_edit,url=url,id_category=id_category)
+        return render_template('editItem.html',item=item_edit,url=url,category_name=category_name)
     
   
-@app.route('/Categories/<int:id_category>/Items/delete/<int:id_item>/')
-def deleteItem(id_item,id_category):
-    item = session.query(Item).filter_by(id=id_item).one()
+@app.route('/catalog/<category_name>/Items/delete/<item_name>/')
+def deleteItem(item_name,category_name):
+    cat = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(Item).filter(and_(Item.name==item_name,Item.category_id==cat.id)).one()
     session.delete(item)
     session.commit()
-    return redirect(url_for('Items',id_category=id_category))
-
-@app.route('/Items/<int:id_item>/')
-def show_item(id_item):
-    ex_item = session.query(Item).filter_by(id=id_item).one()
-    url = photos.url(ex_item.image_name)
-    return render_template("showitem.html",item=ex_item,direc=url)
-
+    return redirect(url_for('Items',category_name=category_name))
 
 def select_last_items():
     tuples =[]
@@ -120,5 +87,5 @@ def select_last_items():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '127.0.0.1',port = 5000)
+    app.run(host = '127.0.0.1',port = 8000)
 
